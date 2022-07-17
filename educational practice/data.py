9f137aad12872a26d6f59f36ae998ae82a5db215
Faner201 <62674880+Faner201@users.noreply.github.com>
2022-07-17 18:10:08 +1000

    Example accessing the fefu website

---
+++ educational practice/data.py
@@ -0,0 +1,31 @@
+from tkinter.messagebox import NO
+from requests import request
+
+
+faculties = ['09.03.03 Прикладная информатика',
+'02.03.03 Математическое обеспечение и администрирование информационных систем', 
+'02.03.01 Математика и компьютерные науки',
+'09.03.04 Программная инженерия', 
+'01.03.02 Прикладная математика и информатика',
+'09.03.02 Информационные системы и технологии',
+'10.03.01 Информационная безопасность',
+'10.05.01 Компьютерная безопасность']
+
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+}
+
+request_parametrs = {
+    'mode': 'class',
+    'c': 'dvfu:admission.spd',
+    'action': 'getStudents'
+}
+
+request_data = {
+    'admissionCampaignType': 'Прием на обучение на бакалавриат/специалитет',
+    'financingSource': 'Бюджетная основа',
+    'studyForm': 'Очная',
+    'implementationPlace': 'Владивосток',
+    'trainingDirection': None,
+    'consent': None
+}
